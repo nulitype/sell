5c18fbbbaf09b4a0d9131e9c883fca7b9b5f3f74
mst0xsd <mst0xsd@gmail.com>
2017-02-11 22:42:50 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <v-header :seller="seller"></v-header>
     <div class="tab">
       <div class="tab-item">
-        <router-link to="/sell/goods">商品</router-link>
+        <router-link :to="{name: 'goods'}">商品</router-link>
       </div>
       <div class="tab-item">
-        <router-link to="/sell/ratings">评论</router-link>
+        <router-link :to="{name: 'ratings'}">评论</router-link>
       </div>
       <div class="tab-item">
-        <router-link to="/sell/seller">商家</router-link>
+        <router-link :to="{name: 'seller'}">商家</router-link>
       </div>
     </div>
     <keep-alive>
--- src/router.js
+++ src/router.js
@@ -5,14 +5,17 @@ import seller from './components/seller/seller';
 export default [
   {
     path: '/sell/goods',
+    name: 'goods',
     component: goods,
   },
   {
     path: '/sell/ratings',
+    name: 'ratings',
     component: ratings,
   },
   {
     path: '/sell/seller',
+    name: 'seller',
     component: seller,
   },
   {
