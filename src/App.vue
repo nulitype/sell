908c76b08a79d4202f5666085c042020e9848792
mst0xsd <mst0xsd@gmail.com>
2017-02-11 22:30:36 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <v-header :seller="seller"></v-header>
     <div class="tab">
       <div class="tab-item">
-        <router-link to="/goods">商品</router-link>
+        <router-link to="/sell/goods">商品</router-link>
       </div>
       <div class="tab-item">
-        <router-link to="/ratings">评论</router-link>
+        <router-link to="/sell/ratings">评论</router-link>
       </div>
       <div class="tab-item">
-        <router-link to="/seller">商家</router-link>
+        <router-link to="/sell/seller">商家</router-link>
       </div>
     </div>
     <keep-alive>
